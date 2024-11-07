d60cd96da09742f1d12d044a5cb227e12aa7b2f7
Fabricyo Barreto <bass.barreto@gmail.com>
2024-11-07 19:46:11 -0300

    style: add backgroundImg to SignIn screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
+import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -10,11 +11,7 @@ export default function App() {
   return (
     <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? (
-        <Center flex={1} bg="$gray700">
-          <Text color="white">Home</Text>
-        </Center>
-      ) : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </GluestackUIProvider>
   );
 }
