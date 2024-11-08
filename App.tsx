207dfd2ee420bd731791de5b5e41e3e62b9a1747
Fabricyo Barreto <bass.barreto@gmail.com>
2024-11-07 21:13:35 -0300

    style: add SignUp screen

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
 import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -11,7 +12,7 @@ export default function App() {
   return (
     <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </GluestackUIProvider>
   );
 }
