13af5ebf9104fff4bcfad702b55f305fd5061360
Fabricyo Barreto <bass.barreto@gmail.com>
2024-11-08 17:56:58 -0300

    style: finalized HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,12 +1,22 @@
-import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { Heading, HStack, Text, VStack, Icon } from "@gluestack-ui/themed";
+import { LogOut } from 'lucide-react-native'
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
-    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center">
-      <VStack>
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto
+        source={{ uri: "https://github.com/gunners-pro.png" }}
+        alt="foto de perfil"
+        w="$16"
+        h="$16"
+      />
+      <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">Olá, </Text>
         <Heading color="$gray100" fontSize="$md">Fabricyo Barreto</Heading>
       </VStack>
+
+      <Icon as={LogOut} color="$gray200" size="xl" />
     </HStack>
   );
 }
