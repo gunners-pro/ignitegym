a6d3c92f5786301f7204e369728129d510bf9c1d
Fabricyo Barreto <bass.barreto@gmail.com>
2024-11-09 22:13:38 -0300

    feat: add a function to alter userphoto in profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -3,9 +3,46 @@ import { Input } from '@components/Input';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 import { Center, Heading, Text, VStack } from '@gluestack-ui/themed';
-import { ScrollView, TouchableOpacity } from 'react-native';
+import { Alert, ScrollView, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import { useState } from 'react';
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState(
+    'https://github.com/gunners-pro.png',
+  );
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+      const photoURI = photoSelected.assets[0].uri;
+      if (photoURI) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as {
+          size: number;
+        };
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return Alert.alert(
+            'Essa imagem é muito grande. Escolha uma de até 5MB',
+          );
+        }
+
+        setUserPhoto(photoURI);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -13,12 +50,12 @@ export function Profile() {
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: 'https://github.com/gunners-pro.png' }}
+            source={{ uri: userPhoto }}
             alt="Imagem do usuário"
             size="xl"
           />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
