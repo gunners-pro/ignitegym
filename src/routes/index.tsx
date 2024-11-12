77adb1185559e1e37de5d51f3ca19a264732597a
Fabricyo Barreto <bass.barreto@gmail.com>
2024-11-11 22:05:50 -0300

    feat: SignUp configuring react hook form to send input data

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,18 @@
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
-import { AuthRoutes } from './auth.routes'
-import { gluestackUIConfig } from '../../config/gluestack-ui.config'
-import { Box } from '@gluestack-ui/themed'
-import { AppRoutes } from './app.routes'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { AuthRoutes } from './auth.routes';
+import { gluestackUIConfig } from '../../config/gluestack-ui.config';
+import { Box } from '@gluestack-ui/themed';
+import { AppRoutes } from './app.routes';
 
 export function Routes() {
-  const theme = DefaultTheme
-  theme.colors.background = gluestackUIConfig.tokens.colors.gray700
+  const theme = DefaultTheme;
+  theme.colors.background = gluestackUIConfig.tokens.colors.gray700;
 
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AppRoutes />
+        <AuthRoutes />
       </NavigationContainer>
     </Box>
-  )
+  );
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,20 +1,37 @@
-import { Center, Heading, Image, ScrollView, Text, VStack } from "@gluestack-ui/themed";
+import {
+  Center,
+  Heading,
+  Image,
+  ScrollView,
+  Text,
+  VStack,
+} from '@gluestack-ui/themed';
 
-import BackgroundImg from '@assets/background.png'
-import Logo from '@assets/logo.svg'
-import { Input } from "@components/Input";
-import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
+import BackgroundImg from '@assets/background.png';
+import Logo from '@assets/logo.svg';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import { Controller, useForm } from 'react-hook-form';
 
 export function SignUp() {
-  const navigator = useNavigation()
+  const navigator = useNavigation();
+  const { control, handleSubmit } = useForm();
 
   function handleGoBack() {
-    navigator.goBack()
+    navigator.goBack();
+  }
+
+  function handleSignUp(data: any) {
+    console.log(data);
   }
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+    <ScrollView
+      contentContainerStyle={styles.contentContainerStyle}
+      showsVerticalScrollIndicator={false}
+    >
       <VStack flex={1} bg="$gray700">
         <Image
           source={BackgroundImg}
@@ -27,27 +44,75 @@ export function SignUp() {
         <VStack flex={1} px="$10" pb="$16">
           <Center my="$24">
             <Logo />
-            <Text color="$gray100" fontSize="$sm">Treine sua mente e o seu corpo</Text>
+            <Text color="$gray100" fontSize="$sm">
+              Treine sua mente e o seu corpo
+            </Text>
           </Center>
 
           <Center gap="$2" flex={1}>
             <Heading color="$gray100">Crie sua conta</Heading>
 
-            <Input placeholder="Nome" />
-            <Input
-              placeholder="E-mail"
-              keyboardType="email-address"
-              autoCapitalize="none"
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Nome"
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
             />
 
-            <Input placeholder="Senha" secureTextEntry />
-            <Button title="Criar e acessar" />
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha"
+                  secureTextEntry
+                  onSubmitEditing={handleSubmit(handleSignUp)}
+                  returnKeyType="send"
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+            />
+
+            <Button
+              title="Criar e acessar"
+              onPress={handleSubmit(handleSignUp)}
+            />
           </Center>
 
-          <Button title="Voltar para o login" variant="outline" mt="$12" onPress={handleGoBack} />
-
+          <Button
+            title="Voltar para o login"
+            variant="outline"
+            mt="$12"
+            onPress={handleGoBack}
+          />
         </VStack>
       </VStack>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  contentContainerStyle: {
+    flexGrow: 1,
+  },
+});
